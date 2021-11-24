e8ac145632b14312dd0adbd78bbb9eceec58a4a8
Joseph Hunter <jrhunt1706@gmail.com>
2021-11-23 21:30:30 -0600

    modified activites week 4

--- week4/4.1.4-CreatingElements.js
+++ week4/4.1.4-CreatingElements.js
@@ -1,9 +1,25 @@
 // create a new unordered list (ul) element
-
+const new_Unordered_List = document.createElement("ul");
 // remove the paragraph element in the nav-bar
+let childToRemove = document.querySelector(".nav-bar > p");
+childToRemove
 
 // add your new ul element to the nav-bar
+const unordered = document.createElement("ul");
+
+document.querySelector(".nav-bar").append(unordered);
+
+// append the new element as the last child element to 'main'
+
+document.querySelector("main").appendChild().lastChild;
 
 // create two new list item (li) elements, and add some text to them
 
+const list = document.createElement("li");
+const list1 = document.createElement("li");
+
+listItem1.textContent = "1st new list item";
+listItem2.textContent = "1st new list item";
+
 // add the li elements to the ul in the nav-bar
+
