216bb5d05c664a7bf36c0c1b945c4934b85fe4ca
Joseph Hunter <jrhunt1706@gmail.com>
2021-11-27 11:08:04 -0600

    week 4 activities

---
+++ week4/4.3.4-Reduce.js
@@ -0,0 +1,20 @@
+
+  // using reduce, sum the numbers from 1 to 200
+  // hint: use the arrayOfNumbers() function to generate an array
+  
+  function arrayOfNumbers(end = 100) {
+    const nums = [];
+    for (let i = 1; i <= end; i++) {
+      nums.push(i);
+    }
+    return nums;
+  }
+  
+  // Problem 1 - Using Reduce, sum the number from 1 to 200
+  // Hint: Use the arrayOfNumbers() function to generate an array
+  
+  // Solution 1: (there is more than one solution to this problem)
+  const numsToSum = arrayOfNumbers(200);
+  console.log(numsToSum);
+  const theSum = numsToSum.reduce((acc, num) => acc += num, 0);
+  console.log(theSum);
